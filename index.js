388ed80d7444c802df625e868324150d030dda9b
kaletmk17 <tusharkale581@gmail.com>
2024-07-11 18:14:08 +0530

    sum of odd num program added

--- index.js
+++ index.js
@@ -10,3 +10,16 @@ for (const num of numbers) {
     }
 }
 console.log(large);
+
+
+
+let sum = 0;
+for (const num of numbers)
+{
+    if (num % 2 == 1)
+    {
+        sum = sum + num
+    }
+}
+console.log(sum);
+
